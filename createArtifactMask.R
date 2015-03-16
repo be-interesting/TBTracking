ecb4610521f76722505242d4ffc33e9b915f54eb
Will <procterw@gmail.com>
2015-03-16 16:58:08 -0700

    createArtifactMask works much better now. Includes square deletion and crops out circles more accurately.

--- createArtifactMask.R
+++ createArtifactMask.R
@@ -7,14 +7,15 @@ createArtifactMask <- function(bg) {
   
   # Extract pixel data for speed's sake
   bg.data <- bg@.Data
-  output <- bg@.Data
+  sq.data <- bg@.Data
+  cr.data <- bg@.Data
   
   # Get dimensions
   dim.x <- dim(bg.data)[[1]]
   dim.y <- dim(bg.data)[[2]]
   
   # Set mask to matrix of dim xy
-  outputMask <- matrix(1, dim.x, dim.y)
+  output <- matrix(1, dim.x, dim.y)
   
   # TEST
   # Isolate the y ranges to search to speed things up
@@ -29,36 +30,80 @@ createArtifactMask <- function(bg) {
     # For each y value, search through all of the x values and find similar areas
     for (x in seq(1, dim.x-31, 4)) {
       
+      # Arbitrary threshold
+      thr1 <- 0.039
+      thr2 <- 0.041
+      
       # Get subset of original
       subset <- bg.data[x:(x+30),y:(y+30)]
-      
-      # Arbitrary threshold
-      thr <- 0.04
       
       dx.beg <- mean(abs(diff(subset[15,0:10])))
       dx.mid <- mean(abs(diff(subset[15,12:18])))
       dx.end <- mean(abs(diff(subset[15,20:30])))
+      
       dy.beg <- mean(abs(diff(subset[15,0:10])))
       dy.mid <- mean(abs(diff(subset[15,12:18])))
       dy.end <- mean(abs(diff(subset[15,20:30])))
       
-      if(dx.mid < thr & dy.mid < thr # low change in mids
-         & dx.beg - dx.end < thr & dy.beg - dy.end < thr # beg similar to end
-         & dx.beg > thr & dx.end > thr # high change in beg/end
-         & dy.beg > thr & dx.end > thr # high change in beg/end
-         ) {
-        
-        # mask subset with white circle
-        subset <- output[x:(x+30),y:(y+30)] * createFillCircle(31, 31, 10)
-        outputMask[x:(x+30),y:(y+30)] <- outputMask[x:(x+30),y:(y+30)] * createFillCircle(31, 31, 10)
-        subset[subset==0] <- 1
-        
-#         output[x:(x+30),y:(y+30)] <- subset
-        
+      if(dx.mid < 0.039 & dy.mid < 0.039 & dx.beg - dx.end < 0.039 & dy.beg - dy.end < 0.5 & dx.beg > 0.039
+         & dx.end > 0.039 & dy.beg > 0.039 & dx.end > 0.039) {
+          # mask subset with white circle
+          subset <- cr.data[x:(x+30),y:(y+30)] * createFillCircle(31, 31, 9)
+          output[x:(x+30),y:(y+30)] <- output[x:(x+30),y:(y+30)] * createFillCircle(31, 31, 9)
       }
-    
+      
+
+      sq.x <- 230
+      sq.y <- 230 
+      
+      if (x < dim.x - sq.x & y < dim.y - sq.y) {
+        
+        # Get subset of original
+        subset <- bg.data[x:(x+(sq.x-1)),y:(y+sq.y-1)]
+        
+        perDark <- function(m) {
+          return(sum(m<0.5, na.rm=TRUE) / (dim(m)[[1]] * dim(m)[[2]]))
+        }
+        
+        isDarker <- function(a,b) {
+          return(perDark(a) - perDark(b) > 0.28)
+        }
+        
+        # compare y strips
+        y11 <- subset[0:20, ]
+        y12 <- subset[20:40, ]
+        y21 <- subset[(sq.x-20):sq.x, ]
+        y22 <- subset[(sq.x-40):(sq.x-20), ]
+        
+        # compare x strips
+        x11 <- subset[ ,0:20]
+        x12 <- subset[ ,20:40]
+        x21 <- subset[ ,(sq.x-20):sq.x]
+        x22 <- subset[ ,(sq.x-40):(sq.x-20)]
+        
+        # get center
+        cx1 <- round(sq.x/2 - 10)
+        cx2 <- round(sq.x/2 + 10)
+        cy1 <- round(sq.y/2 - 10)
+        cy2 <- round(sq.y/2 + 10)
+        center <- subset[cx1:cx2,cy1:cy2]
+        
+        
+        if (isDarker(y12,y11) & isDarker(y22,y21) & isDarker(x12, x11) & isDarker(x22, x21)
+            & perDark(center) < 0.3) {
+          # mask subset with square
+          sq.mask <- matrix(0, sq.x, sq.y)
+          sq.mask[1:6, ] <- 1
+          sq.mask[(sq.x-6):sq.x, ] <- 1
+          sq.mask[,1:6] <- 1
+          sq.mask[,(sq.y-6):sq.y] <- 1
+          output[x:(x+(sq.x-1)),y:(y+sq.y-1)] <- output[x:(x+(sq.x-1)),y:(y+sq.y-1)] * sq.mask
+        }
+      }
+        
     }
   }
-  
-  return(outputMask)
+
+
+  return(output)
 }
--- main.R
+++ main.R
@@ -8,17 +8,33 @@ source("createArtifactMask.R")
 # "Levels" algorithm to isolate bacteria as black pixels
 # Statistically group black clumps together
 
-# read in the background frame for testing
+# # read in the background frame for testing
 frame.bg <- readImage("data/background.tif")
+# 
+# dark <- frame.bg@.Data < 0.32
+# frame.1[dark] <- 1
+# 
+# 
+# dark[dark==0] <- 1
+# display(dark)
+# 
+# frame.1 <- readImage("data/frame1.tif")
+# 
+# 
+# 
+# test <- frame.1 * dark
+
 
 # This definitely works
-test <- createArtifactMask(bg)
+test <- createArtifactMask(frame.bg)
 test <- frame.bg * test
 test[test==0] <- 1
 
+# sq <- readImage("examples/d1.tif")
+# display(sq[22:237, 5:220])
 
 
-
+#215 #215
 
 
 
