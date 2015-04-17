8d0f593ff4c7663059da2fd36d23bdf8144b6723
Will Leahy <willl@shuksan.domain>
2015-04-17 15:29:33 -0700

    Added alternate (but not necessarily better) approach to image recognition

--- scripts/isolateBacteria.R
+++ scripts/isolateBacteria.R
@@ -1,5 +1,5 @@
 # Returns a black and white mask of where bacteria can be found
-isolateBacteria <- function(m) {
+isolateBacteriaOld <- function(m) {
   
   # Create mask of where dark lines are unlikely to contain bacteria
   lineMasks <- darkLineMask(m)
@@ -42,3 +42,46 @@ isolateBacteria <- function(m) {
   return(bwlabel(h))
   
 }
+
+
+
+
+isolateBacteria <- function(m) {  
+  
+  # Create mask of where dark lines are unlikely to contain bacteria
+  lineMasks <- darkLineMask(m)
+  
+  # Darken areas not in the line mask
+  m[lineMasks$inverse] <- m[lineMasks$inverse] / 1.5
+  
+  # Create glcm masks
+  a <- glcm(m, n_grey=20, shift=list(c(0,1),c(1,0),c(1,1)), 
+            window=c(3,3), min_x=0, max_x=0.35)
+  
+  b <- (m * a[,,3]) < 0.2
+  
+
+  
+  b[artifactMask>0] <- 0
+  b[lineMasks$normal==1] <- 0
+  
+  
+  
+  # Mild dilate/erode to close small gaps
+  kern <- makeBrush(5, shape='disc')
+  d <- dilateGreyScale(b, kern)
+  d <- erodeGreyScale(d, kern)
+  
+  # Remove small blobs
+  e <- removeBlobs(d, 50)
+  
+  kern <- makeBrush(5, shape='disc')
+  # run dilate, mask, and remove smaller blobs
+  f <- dilateGreyScale(e, kern)
+  f <- erodeGreyScale(f, kern)
+  g <- f * (m < 0.4)
+  h <- removeBlobs(g, 15)  
+  
+  return(bwlabel(h))
+  
+  }
