4c8bbb82a9f4dce203f7ea2a13981597ae635779
Will <procterw@gmail.com>
2015-05-15 15:48:39 -0700

    Wrote script which creates a gif from a list of matrices. Requires ImageMagick

--- scripts/makeGif.R
+++ scripts/makeGif.R
@@ -2,9 +2,25 @@
 
 
 
-
-function makeGif <- function() {
+# Accepts a list of matrices and creates a gif
+makeGif <- function(frames, filename, delay, resize=100) {
   
+  # Temporary directory location. We store frames here
+  tempDir <- "temp12342345/"
   
+  dir.create(tempDir)
+  
+  # Save each image as a tiff frame
+  for (i in 1:length(frames)) {
+    j <- ifelse(i<10,paste0("0",i),i)
+    writeImage(frames[[i]],paste0(tempDir,j,".tiff"))
+  }
+  
+  # Convert frames to gif
+  system(paste0('convert -resize "', resize, '%" ', tempDir, '*.tiff ',
+                filename))
+  
+  # Delete temporary directory
+  unlink(tempDir, recursive=TRUE)
   
 }
